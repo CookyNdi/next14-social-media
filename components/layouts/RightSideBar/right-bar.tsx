126aaaed8d7f2d58154c2552d4ba232e52d8c5b2
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2023-10-31 13:45:20 +0700

    feat : left bar content

--- components/layouts/RightSideBar/right-bar.tsx
+++ components/layouts/RightSideBar/right-bar.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import SearchComponent from '@/components/Search';
 
 const RightBar = () => {
   return (
-    <div className='w-[25%] h-full fixed top-0 right-0 bg-neutral-900 hidden md:flex flex-col justify-between px-6 py-4'>
-
+    <div className='w-[30%] h-full fixed top-0 right-0 border-l border-neutral-700 bg-neutral-900 hidden md:flex flex-col justify-between px-6 py-4'>
+      <SearchComponent />
     </div>
-  )
-}
+  );
+};
 
-export default RightBar
+export default RightBar;
