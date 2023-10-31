91a5135549e3e408cfb7e1e3a2e3df807ae10692
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2023-10-31 13:46:25 +0700

    chore : add letter spacing

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,7 +9,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       <button
         type={type}
         className={twMerge(
-          `w-full rounded-full bg-purple-600 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-neutral-200 font-bold hover:opacity-80 transition`, className
+          `w-full rounded-full bg-purple-600 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-neutral-200 font-bold hover:opacity-80 transition tracking-widest`, className
         )}
         disabled={disabled}
         ref={ref}
