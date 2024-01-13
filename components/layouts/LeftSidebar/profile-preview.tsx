4afc9444b9737f80e179849c6e198443c584b5fe
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-01-13 11:46:41 +0700

    feat : logout button

--- components/layouts/LeftSidebar/profile-preview.tsx
+++ components/layouts/LeftSidebar/profile-preview.tsx
@@ -1,26 +1,37 @@
-import { IoIosArrowDown } from 'react-icons/io';
+import { HiOutlineDotsHorizontal } from 'react-icons/hi';
 
 import { auth } from '@/auth';
 import ProfileImage from '@/components/profile-image';
-import { cn } from '@/lib/utils';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { LogoutButton } from '@/components/auth/logout-button';
 
 const ProfilePreview = async () => {
   const session = await auth();
   return (
-    <button
-      className={cn(
-        'flex justify-between items-center gap-x-2 rounded-3xl pl-4 text-neutral-400 text-base font-medium transition-all hover:bg-neutral-500/20'
-      )}
-    >
-      <div className='flex items-center gap-x-4 py-2'>
-        <ProfileImage />
-        <div className='flex flex-col text-left'>
-          <h1 className='w-32 text-neutral-200 text-base truncate'>{session?.user?.name || 'Example'}</h1>
-          <h1 className='text-neutral-400 text-sm'>{session?.user?.username || '@Example'}</h1>
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className='flex justify-between items-center gap-x-2 rounded-3xl pl-4 text-neutral-400 text-base font-medium transition-all hover:bg-neutral-500/20 cursor-pointer'>
+          <div className='flex items-center gap-x-4 py-2'>
+            <ProfileImage />
+            <div className='flex flex-col text-left'>
+              <span className='w-32 text-neutral-200 text-base truncate'>{session?.user?.name || 'Example'}</span>
+              <span className='text-neutral-400 text-sm'>{session?.user?.username || '@Example'}</span>
+            </div>
+          </div>
+          <HiOutlineDotsHorizontal size={24} className='mr-4' />
         </div>
-      </div>
-      <IoIosArrowDown size={24} className='mr-4' />
-    </button>
+      </DialogTrigger>
+      <DialogContent className='max-w-[320px] sm:max-w-md'>
+        <div className='pt-4'>
+          <LogoutButton>
+            <Button variant='purple' className='w-full'>
+              Logout
+            </Button>
+          </LogoutButton>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 };
 
--- components/layouts/RightSideBar/right-bar.tsx
+++ components/layouts/RightSideBar/right-bar.tsx
@@ -1,30 +1,23 @@
-import React from 'react';
-import SearchComponent from '@/components/Search';
-import TrendsForYou from './trends-for-you';
-import Footter from './footer';
-import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+
+import SearchComponent from '@/components/Search';
+import TrendsForYou from '@/components/layouts/RightSideBar/trends-for-you';
+import Footter from '@/components/layouts/RightSideBar/footer';
+import { Button } from '@/components/ui/button';
 import { auth } from '@/auth';
-import { LogoutButton } from '@/components/auth/logout-button';
 
 const RightBar = async () => {
   const session = await auth();
 
   return (
-    <div className='w-[30%] min-h-screen fixed top-0 right-0 border-l border-neutral-700 bg-neutral-900 hidden md:flex flex-col gap-y-6 px-6 py-4'>
+    <div className='w-[30%] min-h-screen fixed top-0 right-0 border-l border-neutral-700 bg-neutral-900 hidden lg:flex flex-col gap-y-6 px-6 py-4'>
       <SearchComponent />
       <TrendsForYou />
       <Footter />
-      {!session?.user ? (
+      {!session?.user && (
         <Button variant='purpleSecondary' className='mt-4'>
           <Link href='/auth/login'>Login</Link>
         </Button>
-      ) : (
-        <LogoutButton>
-          <Button variant='purpleSecondary' className='mt-4'>
-            Logout
-          </Button>
-        </LogoutButton>
       )}
     </div>
   );
