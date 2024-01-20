8f2693b927951aea8316992e20cfde84def054a5
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-01-20 08:15:34 +0700

    feat : nav dynamic routes

--- components/layouts/MobileNavbar/index.tsx
+++ components/layouts/MobileNavbar/index.tsx
@@ -1,12 +1,14 @@
 'use client';
-import React from 'react';
-import { routes } from '../LeftSidebar/sidebar-routes';
-import NavItem from './navItem';
+import { useCurrentUser } from '@/hooks/useCurrentUser';
+import { navRoutes } from '@/lib/navRoutes';
+import NavItem from '@/components/layouts/MobileNavbar/navItem';
 
 const MobileNavbar = () => {
+  const user = useCurrentUser();
+  navRoutes[3].href = user!.username.slice(1);
   return (
     <div className='w-full h-14 border-t border-neutral-700 fixed bottom-0 z-[100] left-0 bg-neutral-900 flex lg:hidden'>
-      {routes.map((route, index) => (
+      {navRoutes.map((route, index) => (
         <NavItem href={route.href} label={route.label} icon={route.icon} key={index} />
       ))}
     </div>
